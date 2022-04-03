dc65d9f9ce4d28335f8067f6fdb64b272490b4ca
xjn-xjn <xjnxjn@aol.com>
2022-04-03 17:08:44 -0500

    added fib3 base case in dictionary key value pairs

--- chapter_01/fib3.py
+++ chapter_01/fib3.py
@@ -1,1 +1,11 @@
 # TODO: write fib3.py
+
+from typing import Dict
+
+# establish base case in a dictionary
+# using key value pairs
+memo: Dict[int, int] = {0:0, 1:1}
+
+if __name__ == "__main__":
+    print("key: 0, val: " + str(memo[0]))
+    print("key: 1, val: " + str(memo[1]))
