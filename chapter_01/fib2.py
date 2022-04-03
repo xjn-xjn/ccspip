f51e6d48bc106db9df67b66691b83d3ab3ecffa6
xjn-xjn <xjnxjn@aol.com>
2022-04-03 16:59:45 -0500

    added base case to fib2.py to prevent infinite loop

--- chapter_01/fib2.py
+++ chapter_01/fib2.py
@@ -1,6 +1,10 @@
-#TODO: write fib2
-
+def fib2(x):
+    # establish base case to prevent infinite recursion
+    if x < 2:
+        return x
+    return fib2(x-1) + fib2(x-2)
 
 
 if __name__ == "__main__":
+    print(fib2(25))
     print("fib2.py finished running")
