1e71cd63809575150bb96b312b34ca2594ca243d
xjn-xjn <xjnxjn@aol.com>
2022-04-06 16:55:01 -0500

    Add logic to make_array

--- src/size_of_test.py
+++ src/size_of_test.py
@@ -7,7 +7,24 @@ def make_array(j: int, k: int) -> list:
     an list of lists representing an array is returned consisting of 0 through max number array can hold.
   """
   
-  return [j, k]
+  element = 0  # our initial array element
+  array = []
+  # loop through rows
+  for row in range(j):
+    # new row
+    new_row = []
+    # loop through columns
+    for col in range(k):
+      # append element to row
+      new_row.append(element)
+      # increment elemwnt
+      element += 1
+    # append row to array
+    array.append(new_row)
+      
+    
+  
+  return array
   
   
 if __name__ == "__main__":
