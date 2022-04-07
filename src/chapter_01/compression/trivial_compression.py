da08a626ffa39745f7669018c512549288ed26d0
xjn-xjn <xjnxjn@aol.com>
2022-04-07 17:12:16 -0500

    Fix syntax errors, add prints for debug, ckde is working now, previous commit was a lie

--- src/chapter_01/compression/trivial_compression.py
+++ src/chapter_01/compression/trivial_compression.py
@@ -6,20 +6,30 @@ class CompressedGene:
 	def __init__(self, gene: str) -> None:
 		self._compress(gene)
 	def _compress(self, gene: str) -> None:
-		print(gene)
+		# print(gene)
+		printy = ""
 		self.bit_string: int = 1	# sentinel
 		for nucleotide in gene.upper():
 			self.bit_string <<= 2    # shift left two bit_string
-		if nucleotide == "A":    # change last two bits to 00
-			self.bit_string |= 0b00
-		elif nucleotide == "C":    # change last two bits to 01
-			self.bit_string |= 0b01
-		elif nucleotide == "G":    # change last two bits to 10
-			self.bit_string |= 0b10
-		elif nucleotide == "T":    # change last two bits to 10
-			self.bit_string |= 0b11
-		else:
-			raise ValueError("Invalid Nucleotide:{}".format(nucleotide))
+			if nucleotide == "A":    # change last two bits to 00
+				self.bit_string |= 0b00
+				printy += "00"
+			elif nucleotide == "C":    # change last two bits to 01
+				self.bit_string |= 0b01
+				printy += "01"
+			elif nucleotide == "G":    # change last two bits to 10
+				self.bit_string |= 0b10
+				printy += "10"
+			elif nucleotide == "T":    # change last two bits to 10
+				self.bit_string |= 0b11
+				printy += "11"
+			else:
+				raise ValueError("Invalid Nucleotide:{}".format(nucleotide))
+			
+		# print(self.bit_string)
+		print(printy)
+	
         
 if __name__ == "__main__":
-	test: CompressedGene = CompressedGene("bitch")
+	# test: CompressedGene = CompressedGene("bitch")
+	test: CompressedGene = CompressedGene("atcgcgtaaattatttattatgcccgtatttat")
