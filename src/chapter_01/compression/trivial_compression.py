d722ac514c6476bf3b5f3e945a38d1eba8191bbf
xjn-xjn <xjnxjn@aol.com>
2022-04-07 16:14:47 -0500

    fix shitty code that DID NOT RUN until now :(

--- src/chapter_01/compression/trivial_compression.py
+++ src/chapter_01/compression/trivial_compression.py
@@ -5,21 +5,21 @@
 class CompressedGene:
 	def __init__(self, gene: str) -> None:
 		self._compress(gene)
-        
-    def _compress(gene: str) -> None:
-        self.bit_string: int = 1    # sentinel
-        for nucleotide in gene.upper():
-        	self.bit_string <<= 2    # shift left two bit_string
-        	if nucleotide == "A":    # change last two bits to 00
-        		self.bit_string |= 0b00
-        	elif nucleotide == "C":    # change last two bits to 01
-        		self.bit_string |= 0b01
-        	elif nucleotide == "G":    # change last two bits to 10
-        		self.bit_string |= 0b10
-        	elif nucleotide == "T":    # change last two bits to 10
-        		self.bit_string |= 0b11
-        	else:
-        		raise ValueError("Invalid Nucleotide:{]".format(nucleotide))
+	def _compress(self, gene: str) -> None:
+		print(gene)
+		self.bit_string: int = 1	# sentinel
+		for nucleotide in gene.upper():
+			self.bit_string <<= 2    # shift left two bit_string
+		if nucleotide == "A":    # change last two bits to 00
+			self.bit_string |= 0b00
+		elif nucleotide == "C":    # change last two bits to 01
+			self.bit_string |= 0b01
+		elif nucleotide == "G":    # change last two bits to 10
+			self.bit_string |= 0b10
+		elif nucleotide == "T":    # change last two bits to 10
+			self.bit_string |= 0b11
+		else:
+			raise ValueError("Invalid Nucleotide:{}".format(nucleotide))
         
 if __name__ == "__main__":
-	test: CompressedGene = CompressedGene()
+	test: CompressedGene = CompressedGene("bitch")
