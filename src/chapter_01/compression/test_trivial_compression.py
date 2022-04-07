6f036413f0f9ea2822409197787fe64b3108f4d0
xjn-xjn <xjnxjn@aol.com>
2022-04-07 17:39:19 -0500

    Add pytest

--- src/chapter_01/compression/test_trivial_compression.py
+++ src/chapter_01/compression/test_trivial_compression.py
@@ -1,2 +1,9 @@
 # TODO: write pytests
+import trivial_compression as tc
+import pytest
 
+
+def test_fails():
+	# gene = "NotAGeneStringBitch"
+	with pytest.raises(ValueError) as e_info:
+		test: tc.CompressedGene = tc.CompressedGene("bitch")
